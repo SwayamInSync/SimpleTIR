a83f7328e5fdbf88544b58841608e6e238fca05d
SwayamInSync <hawkempire007@gmail.com>
2025-12-12 17:39:46 +0000

    test after 20

--- run.sh
+++ run.sh
@@ -9,19 +9,20 @@ export CONFIG_NAME=simpletir_trainer
 
 export SANDBOX_ENDPOINT=http://127.0.0.1:12345/faas/sandbox/
 
+
 # Add custom hydra overrides for faster training
-export HYDRA_OVERRIDES="actor_rollout_ref.rollout.n=8"
+export HYDRA_OVERRIDES="actor_rollout_ref.rollout.n=6"
 
 bash train.sh \
-  --max_response_length 8000 \
-  --max_prompt_length 16000 \
+  --max_response_length 4000 \
+  --max_prompt_length 8000 \
   --model_name Qwen2.5-7B \
   --max_turns 3 \
   --train_batch_size 512 \
   --val_sample_size 50 \
-  --n_val 8 \
-  --val_before_train False \
-  --test_freq 9999 \
+  --n_val 4 \
+  --val_before_train True \
+  --test_freq 20 \
   --mask_void_turns False \
   --train_dataset "simplelr_math_35/train"
   # Use only simplelr_math_35 (8.5k samples) for faster experiments
